1cf278735251f02f677eee185460205270003fc4
Pedro Aguiar <pedro-henriqueaguiar@hotmail.com>
2024-02-08 13:29:35 -0300

    feat: Implementing Validator

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpResquest
 from src.views.tag_creator_view import TagCreatorView
-
 from src.errors.error_handler import handle_errors
+from src.validators.tag_creator_validator import tag_creator_validator
 
 tags_routes_bp = Blueprint('tags_routes',__name__)
 
@@ -11,6 +11,8 @@ tags_routes_bp = Blueprint('tags_routes',__name__)
 def create_tags():
     response = None
     try:
+        tag_creator_validator(request)
+
         tag_creator_view = TagCreatorView()
 
         http_request = HttpResquest(body=request.json)
