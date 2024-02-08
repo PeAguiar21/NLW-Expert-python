d925a4df0e731f0c802b8bc4eda30b50b24683b7
Pedro Aguiar <pedro-henriqueaguiar@hotmail.com>
2024-02-08 12:56:07 -0300

    feat: Implementing Error Handler

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpResquest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 
 class TagCreatorView:
@@ -9,11 +10,13 @@ class TagCreatorView:
     '''
 
     def validade_and_create(self, http_request: HttpResquest) -> HttpResponse:
-        #body = http_request.body
-        #product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
 
         # logica de regra de negocio
-        print('Estou na minha view')
-        print(http_request)
+        formatted_response =  tag_creator_controller.create(product_code)
+
         # retorno http
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
